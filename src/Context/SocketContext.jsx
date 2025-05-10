4f03530568f32e18e52401a97f5babd52121d34d
Sania Singla <singlasania7717@gmail.com>
2025-05-11 01:57:21 +0530

    final

--- src/Context/SocketContext.jsx
+++ src/Context/SocketContext.jsx
@@ -2,6 +2,7 @@ import { useContext, createContext, useState, useEffect } from 'react';
 import { io } from 'socket.io-client';
 import { useUserContext } from './UserContext';
 import { useChatContext } from './ChatContext';
+import { BASE_BACKEND_URL } from '../Constants/constants';
 
 const SocketContext = createContext();
 
@@ -14,9 +15,7 @@ const SocketContextProvider = ({ children }) => {
     function connectSocket() {
         if (!user || socket) return;
 
-        const socketInstance = io(import.meta.env.VITE_BACKEND_BASE_URL, {
-            withCredentials: true,
-        });
+        const socketInstance = io(BASE_BACKEND_URL, { withCredentials: true });
 
         socketInstance.on('connect', () => {
             console.log('socket connected. SocketId: ', socketInstance.id);
